cbf131a9bf05c080639a2292e2a4facca1e4f93f
Gerald Kwok <43295484+gdis5251@users.noreply.github.com>
2018-10-30 20:59:16 +0800

    Update 购书排序.c

--- 编程练习/购书排序.c
+++ 编程练习/购书排序.c
@@ -4,16 +4,17 @@
 #include <stdlib.h>
 
 /*
-* ͼ��ݽ�Ҫ���飬Ҫ����N��ͬѧ��˵�Լ���Ҫʲô�顣
-  ÿ����ֻ��һ������Ҫ�����ISBN�Ŵ�С��������
-  �˷���Ϊ�򵥵�Ͱ���򣬼ȿ���ȥ�أ��ֿ�������
-  �������ISBN����1~1000֮�ڡ�
-* ���ķ�
+* 图书馆将要买书，要请来N个同学来说自己想要什么书。
+  每种书只买一本，还要把书的ISBN号从小到大排序。
+  此方法为简单的桶排序，既可以去重，又可以排序。
+  假设书的ISBN好在1~1000之内。
+* 郭文峰
 * 2018/9/26
 */
 
 int main(void)
 {
+
 	int i = 0;
 	int n = 0;
 	int a[1001] = {0};
@@ -21,10 +22,12 @@ int main(void)
 	int count = 0;
 
 	scanf("%d", &n);
-
+         
+	//输入ISBN号并用a数组记录
 	for (i = 1; i <= n; i++)
 	{
 		scanf("%d", &t);
+		//去重
 		a[t] = 1;
 	}
 
@@ -33,6 +36,7 @@ int main(void)
 		if (a[i] == 1)
 		{
 			printf("%d ", i);
+			//count种书
 			count++;
 		}
 	}

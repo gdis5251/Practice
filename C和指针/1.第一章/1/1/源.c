699f27d36d82bef06499925aa7f08a182b69766e
Gerald Kwok <841784596@qq.com>
2018-12-25 21:10:38 +0800

    [郭文峰][写入][2018/12/25] 写了一道阶乘的题，可是题目却非常变态的要求N <= 1000， 当N取1000时，我的做法无论如何都会溢出，有时间在试试非常规做法

--- C和指针/1.第一章/1/1/源.c
+++ C和指针/1.第一章/1/1/源.c
@@ -1,17 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <Windows.h>
 
-/*
-* 输出Hello World!
-* 郭文峰
-* 2018/10/31
-*/
+#define MAX 9999
+
+static void swap(int *x, int *y)
+{
+
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+void print(int *arr, int n)
+{
+	int i_ = 0;
+
+	for (i_ = 0; i_ < n; i_++)
+	{
+		printf("%d ", arr[i_]);
+	}
+}
+
+
+void quickSort(int *arr, int left, int right)
+{
+	if (left >= right)//递归跳出条件
+		return;
+
+	int key = arr[left];
+	int i_ = left;
+	int j_ = right;
+
+	while (i_ < j_)
+	{
+		while (i_ < j_ && arr[j_] >= key)//找一个比基数小的数字
+			j_--;
+
+
+		while (i_ < j_ && arr[i_] <= key)//找一个比基数大的数字
+			i_++;
+
+
+		if (i_ < j_)//满足条件的话交换
+			swap(&arr[i_], &arr[j_]);
+	}
+
+	//将i_=j_的数字与基数交换
+	arr[left] = arr[i_];
+	arr[i_] = key;
+
+	quickSort(arr, left, i_ - 1);
+	quickSort(arr, i_ + 1, right);
+
+}
 
 int main(void)
 {
-	printf("Hello World!\n");
+	int i = 0;
+	int n = 0;
+	scanf("%d", &n);
+
+	int *arr = (int *)malloc(sizeof(int) * n);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	quickSort(arr, 0, n - 1);
+
+	print(arr, n);
+	printf("\n");
+
+	free(arr);
 
 	system("pause");
 	return 0;
